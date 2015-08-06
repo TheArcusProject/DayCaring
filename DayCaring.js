eebd7c7887b3d84c47343aa9287528423453d8d6
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-06 13:44:33 -0500

    fixed whitespace

--- DayCaring.js
+++ DayCaring.js
@@ -1,17 +1,16 @@
+
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
   Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_EMAIL'
-  });
-
-
+  }); 
 
 }
 
 if (Meteor.isServer) {
-
   Meteor.startup(function () {
     schools.insert(schoolsData.data);
     nonCompliance.insert(nonComplianceData.data);
