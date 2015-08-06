ece569ce030f2a8e4bbbbb4a8cf4d80ae53a20c1
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-05 19:48:04 -0500

    updated template file structure, added google maps

--- DayCaring.js
+++ DayCaring.js
@@ -1,25 +1,3 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-  Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_EMAIL'
-    
-  });
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     
