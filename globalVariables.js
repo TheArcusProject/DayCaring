253446c1bdca113bab441bfca928e8e2b642a5b8
john <redford.john.m@gmail.com>
2015-08-06 11:54:46 -0500

    adds ourUsers table, removes template counter.html, whitespace fiddling

--- globalVariables.js
+++ globalVariables.js
@@ -1,3 +1,3 @@
 schools = new Mongo.Collection('schools');
 nonCompliance = new Mongo.Collection('nonCompliance');
-users = new Mongo.Collection('users');
+ourUsers = new Mongo.Collection('ourUsers');
