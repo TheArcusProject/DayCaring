aa179bec3a3fec942c7aadce7964e981c78ad61c
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-06 15:28:47 -0500

    cleaned up daycaring.js, removed unnecessary lines

--- DayCaring.js
+++ DayCaring.js
@@ -1,13 +1,3 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_EMAIL'
-  }); 
-
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     schools.insert(schoolsData.data);
