9563e2d9ba4d9ae11cbf56f071e03fb9fac03657
colin-h <colinharman@utexas.edu>
2015-08-07 12:09:54 -0500

    testing config done

---
+++ tests/jasmine/client/integration/layoutSpec.js
@@ -0,0 +1,21 @@
+
+//Main Template Tests
+
+describe("main", function(){
+  describe("template", function(){
+    it("has a footer", function(){
+      expect($('.footer')).toBeDefined();
+    });
+  });
+});
+
+describe("searchbar", function(){
+  it("should reroute on submit of search", function(){
+
+    $("form").trigger("submit")
+    expect()
+
+
+  });
+});
+
