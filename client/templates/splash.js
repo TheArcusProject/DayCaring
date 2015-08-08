3851eefbcde98fa07f1c38df80c2ea64be42bc13
john <redford.john.m@gmail.com>
2015-08-08 16:06:02 -0500

    cleanup logs and zipCodes file

--- client/templates/splash.js
+++ client/templates/splash.js
@@ -10,7 +10,6 @@ if (Meteor.isClient) {
 
     "submit form": function(event, template){
       // client side global variable to store user's zip code
-      console.log('user is ', user)
       Meteor.users.update({'_id':user._id},{'$set': {'profile.zip':event.target.zipcode.value}});
       event.preventDefault();
       Meteor.subscribe("zipCodes", event.target.zipcode.value, function(){
