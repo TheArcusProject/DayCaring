8059ae9568fef62a4449f56cb6371c00c8f9cbf3
john <redford.john.m@gmail.com>
2015-08-10 17:51:57 -0500

    removed db population on startup

--- server/DayCaring.js
+++ server/DayCaring.js
@@ -1,21 +1,21 @@
 //DayCaring.js
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  // Meteor.startup(function () {
     
-    console.log('removing schools');
-    schools.remove({});
-    for (var i = 0; i < schoolsData.data.length; i++){
-      schools.insert(schoolsData.data[i]);
-    }
-    console.log('schoolsData is loaded on server'); 
+  //   console.log('removing schools');
+  //   schools.remove({});
+  //   for (var i = 0; i < schoolsData.data.length; i++){
+  //     schools.insert(schoolsData.data[i]);
+  //   }
+  //   console.log('schoolsData is loaded on server'); 
   
-    console.log('removing non-compliance data');
-    nonCompliance.remove({});
+  //   console.log('removing non-compliance data');
+  //   nonCompliance.remove({});
 
-    for (var i = 0; i < nonComplianceData.data.length; i++){
-      nonCompliance.insert(nonComplianceData.data[i]);
-    } 
-    console.log('nonComplianceData is loaded on server');
-  });
+  //   for (var i = 0; i < nonComplianceData.data.length; i++){
+  //     nonCompliance.insert(nonComplianceData.data[i]);
+  //   } 
+  //   console.log('nonComplianceData is loaded on server');
+  // });
 }
 
--- server/zipCodeFileParse.js
+++ server/zipCodeFileParse.js
@@ -1,14 +1,14 @@
 
 //zipCodeParse.js
-Assets.getText('zips.txt',function(err, data){
+// Assets.getText('zips.txt',function(err, data){
   
-  var zipCodesArr = JSON.parse(data);
+//   var zipCodesArr = JSON.parse(data);
   
-  console.log('removing zip Codes');
-  zipCodes.remove({});
-  for (var i = 0; i < zipCodesArr.length; i++){
-    zipCodes.insert(zipCodesArr[i]);
-  };
-  console.log('Zip Code collection is ready on server. Size: ',zipCodesArr.length);
+//   console.log('removing zip Codes');
+//   zipCodes.remove({});
+//   for (var i = 0; i < zipCodesArr.length; i++){
+//     zipCodes.insert(zipCodesArr[i]);
+//   };
+//   console.log('Zip Code collection is ready on server. Size: ',zipCodesArr.length);
 
-})
+// })
