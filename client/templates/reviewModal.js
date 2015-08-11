cc2e6e53ae63f44c33a819fcca34303f9ffcd6ae
Gregg Moore <gregg74@gmail.com>
2015-08-11 15:08:04 -0500

    created ability to store comments in a collection

--- client/templates/reviewModal.js
+++ client/templates/reviewModal.js
@@ -8,13 +8,25 @@ if (Meteor.isClient){
           daycare = daycares[i]
         }
       }
-
   		return daycare[11]
-  		
   	}
   });
+
+  Template.reviewModal.events({
+    "click .button": function(event, template) {
+      event.preventDefault();
+      Meteor.subscribe("reviews", function() {
+        var comment = $('textarea#review').val();
+        reviews.insert({
+          comment: comment,
+          createdAt: new Date()
+        })
+      })
+    }
+  })
 
   Template.reviewModal.onCreated(function(){
     Meteor.subscribe("localSchools", localStorage.getItem('lat'), localStorage.getItem('lng')); 
   })
+
 }
