397650140b47a4f4c78db10c69d6d211e9a0a7ff
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-11 15:31:49 -0500

    text message sends correctly

--- server/DayCaring.js
+++ server/DayCaring.js
@@ -1,21 +1,21 @@
 //DayCaring.js
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
+  Meteor.startup(function() {
+
     // console.log('removing schools');
     // schools.remove({});
     // for (var i = 0; i < schoolsData.data.length; i++){
     //   schools.insert(schoolsData.data[i]);
     // }
     // console.log('schoolsData is loaded on server'); 
-  
-  //   console.log('removing non-compliance data');
-  //   nonCompliance.remove({});
 
-  //   for (var i = 0; i < nonComplianceData.data.length; i++){
-  //     nonCompliance.insert(nonComplianceData.data[i]);
-  //   } 
-  //   console.log('nonComplianceData is loaded on server');
+    //   console.log('removing non-compliance data');
+    //   nonCompliance.remove({});
+
+    //   for (var i = 0; i < nonComplianceData.data.length; i++){
+    //     nonCompliance.insert(nonComplianceData.data[i]);
+    //   } 
+    //   console.log('nonComplianceData is loaded on server');
   });
 }
 
@@ -31,11 +31,24 @@ Meteor.methods({
 
     // Send the e-mail
     Email.send({
-        to: doc.email,
-        from: doc.email,
-        subject: "Website Contact Form: keep slayin boi",
-        text: text
+      to: doc.email,
+      from: doc.email,
+      subject: "Website Contact Form: keep slayin boi",
+      text: text
     });
-  }
-});
-
+  },
+  sendSMS: function(doc) {
+    var twilio = Twilio('AC528f5b6507742d3b1930a5ef129880d5', '8cb9b6181585aacbaf0e60c54fdef8f3');
+    this.unblock();
+    twilio.sendSms({
+        to: doc.phoneNumber, // any number Twilio can deliver to
+        from: '+18323849792', // must be your Twilio account phone number
+        body: 'sup'
+      }, function(err, responseData) { //executed when a response is received from Twilio
+        if (!err) {
+          // "responseData" is a JavaScript object containing data received from Twilio.
+          console.log(responseData.body); // outputs "here is your confirmaton"
+        }
+      })
+    }
+})
