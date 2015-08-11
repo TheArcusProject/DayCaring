8503c4f17778ec4cd3893d530452698c5e33192e
john <redford.john.m@gmail.com>
2015-08-11 11:11:04 -0500

    removed small class from modal

--- client/templates/reviewModal.js
+++ client/templates/reviewModal.js
@@ -1,18 +1,20 @@
-Template.reviewModal.helpers({
-	daycareName: function(){
-		if (daycare){
-			return daycare[11]
-		}
-	}
-});
+if (Meteor.isClient){
 
-Template.reviewModal.onCreated(function(){
-	var dayCareID = FlowRouter.getParam('daycareId');
-    var daycares = localSchools.find().fetch();
-    for (var i = 0; i < daycares.length; i++) {
-      if (daycares[i][0] === parseInt(dayCareID)) {
-        daycare = daycares[i]
-        console.log(daycare)
+  Template.reviewModal.helpers({
+  	daycareName: function(){
+  		var daycares = localSchools.find().fetch();
+      for (var i = 0; i < daycares.length; i++) {
+        if (daycares[i][0] === parseInt(FlowRouter.getParam('daycareId'))) {
+          daycare = daycares[i]
+        }
       }
-    }
-})
+
+  		return daycare[11]
+  		
+  	}
+  });
+
+  Template.reviewModal.onCreated(function(){
+    Meteor.subscribe("localSchools", localStorage.getItem('lat'), localStorage.getItem('lng')); 
+  })
+}
