afa082ff2c94fd5d9171658860aa3690dafb36ec
Gregg Moore <gregg74@gmail.com>
2015-08-12 16:53:08 -0500

    added ability to add comments to a daycare page

--- client/templates/reviewModal.js
+++ client/templates/reviewModal.js
@@ -1,27 +1,33 @@
 if (Meteor.isClient){
 
-  Template.reviewModal.helpers({
-  	daycareName: function(){
-  		var daycares = localSchools.find().fetch();
-      for (var i = 0; i < daycares.length; i++) {
-        if (daycares[i][0] === parseInt(FlowRouter.getParam('daycareId'))) {
-          daycare = daycares[i]
-        }
+	var daycareName = function(){
+		var daycares = localSchools.find().fetch();
+    for (var i = 0; i < daycares.length; i++) {
+      if (daycares[i][0] === parseInt(FlowRouter.getParam('daycareId'))) {
+        var daycare = daycares[i]
       }
-      
-  		return daycare[11];
+    }
+		return daycare[11];
+	}
 
-  	}
-  });
+  Template.reviewModal.helpers({
+    name: function() {
+      return daycareName();
+    }
+  })
 
   Template.reviewModal.events({
     "click .button": function(event, template) {
       event.preventDefault();
       Meteor.subscribe("reviews", function() {
         var comment = $('textarea#review').val();
+        var currentUser = Meteor.user();
+        var daycare = daycareName();
         reviews.insert({
           comment: comment,
-          createdAt: new Date()
+          user: currentUser,
+          daycare: daycare,
+          createdAt: new Date().toDateString()
         })
       })
     }
