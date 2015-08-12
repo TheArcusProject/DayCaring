af095d48232534f466d2e08c900d69d1f158ef66
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-11 19:29:06 -0500

    huzzah email/text verification code sends properly

--- server/DayCaring.js
+++ server/DayCaring.js
@@ -18,9 +18,7 @@ if (Meteor.isServer) {
     //   console.log('nonComplianceData is loaded on server');
   });
 }
-var returnAuth = function(num){
-      return num
-    }
+var code;
 Meteor.methods({
   sendEmail: function(doc) {
     // Important server-side check for security and data integrity
@@ -28,7 +26,8 @@ Meteor.methods({
 
     // Build the e-mail text
     var num = Math.floor(Math.random() * 90000) + 10000;
-    var text = "Email: " + doc.email + "\n\n" + "Here's your validation code: " + num
+    code = num
+    var text = "Email: " + doc.email + "\n\n" + "Here's your validation code: " + code
     this.unblock();
     // Send the e-mail
     Email.send({
@@ -41,12 +40,12 @@ Meteor.methods({
   sendSMS: function(doc) {
     var twilio = Twilio('AC528f5b6507742d3b1930a5ef129880d5', '8cb9b6181585aacbaf0e60c54fdef8f3');
     var num = Math.floor(Math.random() * 90000) + 10000;
-    var auth = returnAuth(num)
+    code = num
     this.unblock();
     twilio.sendSms({
       to: doc.phoneNumber, // any number Twilio can deliver to
       from: '+18323849792', // must be your Twilio account phone number
-      body: "Here's your verification code: " + num
+      body: "Here's your verification code: " + code
     }, function(err, responseData) { //executed when a response is received from Twilio
       if (!err) {
         // "responseData" is a JavaScript object containing data received from Twilio.
@@ -54,7 +53,14 @@ Meteor.methods({
       }
     })
   },
-  authToken: function(){
-    return auth
+  checkValidation: function(doc){
+    if (doc.code === code.toString()) {
+      console.log('checks out!')
+    }
+    else {
+      console.log("nope")
+      console.log("this is doc", doc.code)
+      console.log("this is code", code)
+    }
   }
 })
