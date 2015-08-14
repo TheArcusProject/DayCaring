cc0f376632ee61d917d494dfb13a6d08031123be
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-14 11:37:07 -0500

    cleanup

--- client/templates/repAuthModal.js
+++ client/templates/repAuthModal.js
@@ -1,16 +1,3 @@
-Schema.validationCode = new SimpleSchema({
-  code: {
-    type: String,
-    label: "5 Digit Validation Code"
-  }
-});
-
-Template.repAuthModal.helpers({
-  codeAuthSchema: function(){
-    return Schema.validationCode;
-  }
-});
-
 Template.repAuthModal.events({
 
   "submit .authForm": function(event){
