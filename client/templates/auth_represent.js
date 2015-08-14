253dec338c532d9c68bd8209ef5dd7eef20cc558
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-14 11:34:00 -0500

    cleaned code up

--- client/templates/auth_represent.js
+++ client/templates/auth_represent.js
@@ -3,41 +3,20 @@ function validateEmail(email) {
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
   return re.test(email);
 }
-Schema = {};
-Schema.emailRep = new SimpleSchema({
-  email: {
-    type: String,
-    regEx: SimpleSchema.RegEx.Email,
-    label: "Email Address"
-  },
-})
-Schema.phoneRep = new SimpleSchema({
-  phoneNumber: {
-    type: String,
-    label: "Phone Number"
-  }
-})
-Template.auth_represent.helpers({
-  emailRepSchema: function() {
-    return Schema.emailRep;
-  },
-  phoneRepSchema: function() {
-    return Schema.phoneRep;
-  }
-});
 
 Template.auth_represent.events({
   "submit .form1": function(event) {
     event.preventDefault();
-
     var email = event.target.email.value;
     if (validateEmail(email)) {
       Meteor.call("sendEmail", email, function(err, results) {
-        if (err) console.log(err);
-
-        event.target.email.value = '';
+        if (err) {
+          toastr.warning("Failed to send!");
+        } else {
+          event.target.email.value = '';
+          $('#repAuthModal').foundation('reveal', 'open');
+        }
       });
-      $('#repAuthModal').foundation('reveal', 'open');
     } else {
       toastr.warning('Not a valid email!');
     }
@@ -49,11 +28,13 @@ Template.auth_represent.events({
     var phone = event.target.phone.value;
     console.log("phone :", phone);
     Meteor.call("sendSMS", phone, function(err, results) {
-      if (err) console.log(err);
-
-      event.target.phone.value = '';
+      if (err) {
+        toastr.warning("Failed to send!")
+      } else {
+        event.target.phone.value = '';
+        $('#repAuthModal').foundation('reveal', 'open');
+      }
     });
-    $('#repAuthModal').foundation('reveal', 'open');
   },
 
 });
