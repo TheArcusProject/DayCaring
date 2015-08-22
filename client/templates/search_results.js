37b6f36dc17a98b9dea3bc29b867d2b477282e63
Zachary Lee <w.zachary.lee@gmail.com>
2015-08-22 17:46:54 -0500

    still trying to work out a solution for multiple filters

--- client/templates/search_results.js
+++ client/templates/search_results.js
@@ -6,6 +6,12 @@
 
  var transportationBool = new ReactiveVar(false);
 
+ var partTimeBool = new ReactiveVar(false);
+
+ var saturdayBool = new ReactiveVar(false);
+
+ var distanceMiles = new ReactiveVar(5)
+
  //new reactive variable daycares array
 
 // Helper functions for the overarching search_results page
@@ -34,14 +40,26 @@ Template.search_results.helpers({
     return daycares.find().fetch();
   },
   getCardDaycares: function(){
-    var status;
+    var transportationStatus, partTimeStatus, daysOfWeek;
     if (transportationBool.get() === true) {
-      status = "Y"
+      transportationStatus = "Y"
     } else {
-      status = "N"
+      transportationStatus = "N"
+    }
+    if (partTimeBool.get() === true) {
+      partTimeStatus = "Y"
+    } else {
+      partTimeStatus = {$or: [{'transportation': "N"}, {'transportation': 'N/A'}]} //to handle cases where transportation isnt Y
+    }
+    if (saturdayBool.get() === true) {
+      daysOfWeek = {$regex: ".*Sat.*"}
+    } else {
+      daysOfWeek = {}
     }
     return daycares.find({
-      'transportation': status
+      'transportation': transportationStatus
+      // 'parttime': partTimeStatus
+      // 'days': daysOfWeek
     }).fetch();
   },
   //for capitalization of names and addresses
@@ -55,15 +73,22 @@ Template.search_results.helpers({
 
 Template.search_results.events({
   "click #transportation": function(event){
-    console.log("it's firing")
     var state = transportationBool.get()
-    console.log('this is the state', state)
     if (!state) {
       transportationBool.set(true)
     } else {
       transportationBool.set(false)
     }
   },
+  // "click #partTimeCare": function(event){
+  //   console.log('its firing')
+  //   var partTimeState = partTimeBool.get()
+  //   if (!partTimeState) {
+  //     partTimeBool.set(true)
+  //   } else {
+  //     partTimeBool.set(false)
+  //   }
+  // },
   "click .card" : function(event){
     event.preventDefault();
     FlowRouter.go('/'+ this.iD);
