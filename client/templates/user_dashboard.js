b014f0acb7e36e985b8d62e98e79ad9438b36236
Gregg Moore <gregg74@gmail.com>
2015-08-24 19:17:17 -0500

    refactored user dashboard page to tabs

--- client/templates/user_dashboard.js
+++ client/templates/user_dashboard.js
@@ -46,3 +46,9 @@ Template.user_dashboard.events({
     Meteor.call('deleteComment',this._id);
   }
 })
+Template.user_dashboard.events({
+  "click #deleteComment": function(e){
+    e.preventDefault();
+    Meteor.call('deleteComment',this._id);
+  }
+})
